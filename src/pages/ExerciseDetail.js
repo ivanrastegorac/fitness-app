2863f3aa9621df713d6cbfec469a0f10d31d1712
Ivan Raštegorac <ivan.rastegorac@ag04.com>
2022-10-09 16:30:53 +0200

    Added new components to ExerciseDetail

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos.js';
+import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
-  return <div>ExerciseDetail</div>;
+  return (
+    <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
+      <Detail />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
+  );
 };
 
 export default ExerciseDetail;
